70c8772092559ea26b7132ab81c0e33f8b1614e3
rdxiang <rachelxiangg@gmail.com>
2014-07-09 20:39:35 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -39,6 +39,7 @@
 - (void)step
 {
    // [_grid blah];
+    NSLog(_grid);
     _generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
     _populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
     
